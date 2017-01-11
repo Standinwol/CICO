7b24b80dc1c79e110da35db88a5def931ec4f3fc
pngiang <hitman110321@gmail.com>
2017-01-11 14:56:22 +0700

    no message

--- app/src/main/java/com/project/intership/cico/ChangePassActivity.java
+++ app/src/main/java/com/project/intership/cico/ChangePassActivity.java
@@ -1,14 +1,13 @@
 package com.project.intership.cico;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import com.project.intership.cico.R;
+import android.os.Bundle;
 
-public class InfoActivity extends AppCompatActivity {
+public class ChangePassActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        setContentView(R.layout.activity_change_pass);
     }
 }
